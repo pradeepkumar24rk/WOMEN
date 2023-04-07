6be105dcb9d54aba0c5bc520cec01ee6d66c9396
PraDeepKuMaR <97429335+pradeepkumar24rk@users.noreply.github.com>
2023-04-07 13:38:40 +0530

    get from db is added

--- FrontEnd/src/component/About.jsx
+++ FrontEnd/src/component/About.jsx
@@ -1,37 +1,62 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
 import './About.css'
 import {motion} from 'framer-motion'
+import { useParams } from 'react-router-dom'
+import axios from 'axios'
 
 const About = () => {
-  const transition={duration:3,type:'spring'}
+
+  const transition={duration:3,type:'spring'};
+
+  const [details,setDetails]=useState({});
+  const params=useParams()
+  console.log("params value :"+params.searchInput);
+  const searchInput=params.searchInput;
+
+  useEffect(()=>{
+    const url=`http://localhost:5001/api/data/women/${searchInput}`
+    console.log(url);
+    axios.get(url)
+    .then((res)=>{
+      setDetails(res.data);
+      
+    })
+    .catch((err)=>{
+      console.log(err);
+    })
+  },[searchInput])
+  console.log(details);
   return (
-    <div className='about' id='About'>
-        <motion.div 
-        initial={{left:'-50%'}}
-        whileInView={{left:'15%'}}        
-        transition={{duration:1,type:'spring'}}
-        className='a-left'>
-            <img src="https://assets.weforum.org/sf_account/image/hcp58FIao9mW04d_2-SFQ30rVe2UQkUcRNtjejWil_A.jpg" alt="" srcet="" />
-            
-        </motion.div>
-        <div className='a-right'>
-          <motion.div
-          initial={{right:'-10%'}}
-          whileInView={{right:'0%'}}        
-          transition={transition}
-           className='a-r'>
-            
-            <span>Susan Wojcicki</span>
-            <span>CEO <span style={{color:'var(--blue)'}}>of Youtube</span> </span>
-            <span>
-            Launching Google AdWords and Google Images: As Google's first marketing manager, Wojcicki played a key role in the launch of Google AdWords, the company's flagship advertising platform, and Google Images, one of the most widely used image search engines on the web. As CEO of YouTube, Wojcicki has overseen the platform's growth into one of the largest and most influential websites on the internet. Under her leadership, YouTube has expanded its user base, increased engagement, and launched several new initiatives to promote diversity and inclusion. Wojcicki has been a vocal advocate for gender equality in the tech industry. She has spoken out against the lack of women in leadership positions and has worked to promote initiatives that support women in tech, such as the YouTube Women's Initiative. Wojcicki and her husband have donated millions of dollars to charitable causes, such as education and women's health. They have also supported several non-profit organizations, including Room to Read, the World Wildlife Fund, and the Wikimedia Foundation. Wojcicki has received numerous awards and honors throughout her career, including being named to Forbes' list of the World's 100 Most Powerful Women and Time magazine's list of the 100 most influential people in the world. She has also been inducted into the American Academy of Arts and Sciences and the National Academy of Engineering.
-            </span>
-          </motion.div>
-          
-        </div>
-    </div>
-  )
-}
+          <div className='about' id='About'>
+              
+                  <motion.div 
+                  initial={{left:'-50%'}}
+                  whileInView={{left:'15%'}}        
+                  transition={{duration:1,type:'spring'}}
+                  className='a-left'>
+                      <img src={details.img} alt="" srcet="" />
+                      
+                  </motion.div>
+                  <div className='a-right'>
+                    <motion.div
+                    initial={{right:'-10%'}}
+                    whileInView={{right:'0%'}}        
+                    transition={transition}
+                    className='a-r'>
+                      
+                      <span>{details.name}</span>
+                      <span>{details.rol}</span>
+                      <span>
+                      {details.desc}
+                      </span>
+                    </motion.div>
+                    
+                  </div>
+            </div> 
+        )
+     }
+
+
 
 export default About
